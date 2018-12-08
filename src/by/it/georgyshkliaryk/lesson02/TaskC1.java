7f5f585f92f9f1d683c9d2872c408007a960a52c
georgyshkliaryk <shkliargosh23@gmail.com>
2018-12-08 12:21:24 +0100

    georgyshkliaryk

--- src/by/it/georgyshkliaryk/lesson02/TaskC1.java
+++ src/by/it/georgyshkliaryk/lesson02/TaskC1.java
@@ -9,8 +9,23 @@ package by.it.georgyshkliaryk.lesson02;
 Вывод (это должна появится в консоли, обратите внимание на пробелы и слово Sum:
 Sum = 60
 
-*/
+*/import java.util.Scanner;
 class TaskC1 {
+    public static void main(String[] args) throws Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int maximum = 0;
+
+        if(N>0) {
+            for (int i = 0; i < 3; i++) {
+                int x = scanner.nextInt();
+                if (maximum < x) maximum = x;
+            }
+            System.out.println(maximum);
+
+        }
+}
 
 
 
